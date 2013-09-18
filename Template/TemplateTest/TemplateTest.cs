41acb140806a6b792f899ab571da634544d0ae3d
Valery <karlsson169@gmail.com>
2013-09-18 23:45:26 +0300

    PlainText_AlwaysRenderPlainText: red

--- Template/TemplateTest/TemplateTest.cs
+++ Template/TemplateTest/TemplateTest.cs
@@ -1,6 +1,7 @@
 ﻿namespace TemplateTest
 {
     using System;
+    using System.IO;
 
     using Moq;
 
@@ -8,6 +9,22 @@
 
     public class TemplateTest
     {
-        
+        [Fact]
+        public void PlainText_AlwaysRenderPlainText()
+        {
+            // arrange
+            IProgrammingLanguage language = new Mock<IProgrammingLanguage>().Object;
+            const string TemplateCode = "abcd";
+            var usings = new string[0];
+            using (var template = new Template(language, TemplateCode, usings))
+            using (var output = new StringWriter())
+            {
+                // act
+                template.Render(output);
+
+                // assert
+                Assert.Equal(TemplateCode, output.ToString());
+            }
+        }
     }
 }
