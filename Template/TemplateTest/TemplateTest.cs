eb0f71621631de0266c22a1f4431f2a840777c41
Valery <karlsson169@gmail.com>
2013-09-19 20:54:23 +0300

    some brackets corresponding tests: green

--- Template/TemplateTest/TemplateTest.cs
+++ Template/TemplateTest/TemplateTest.cs
@@ -81,6 +81,42 @@
                 Assert.Throws<BracketsNotCorrespondsException>(() =>
                     new Template(language, TemplateCode, null));
             }
+
+            [Fact]
+            public void OneOpenTwoClose()
+            {
+                // arrange
+                var language = new Mock<IProgrammingLanguage>().Object;
+                const string TemplateCode = "text bef[%ore %]text aft%]er";
+
+                // act and assert
+                Assert.Throws<BracketsNotCorrespondsException>(() =>
+                    new Template(language, TemplateCode, null));
+            }
+
+            [Fact]
+            public void TwoOpenOneClose()
+            {
+                // arrange
+                var language = new Mock<IProgrammingLanguage>().Object;
+                const string TemplateCode = "tex[%t bef[%ore %]text after";
+
+                // act and assert
+                Assert.Throws<BracketsNotCorrespondsException>(() =>
+                    new Template(language, TemplateCode, null));
+            }
+
+            [Fact]
+            public void NestedBrackets()
+            {
+                // arrange
+                var language = new Mock<IProgrammingLanguage>().Object;
+                const string TemplateCode = "tex[%t bef[%ore %]text a%]fter";
+
+                // act and assert
+                Assert.Throws<BracketsNotCorrespondsException>(() =>
+                    new Template(language, TemplateCode, null));
+            }
         }
     }
 }
