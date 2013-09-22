29eb4ec782517bc4e82b1e54afa6d78d3523f2d1
Valery <karlsson169@gmail.com>
2013-09-22 11:05:08 +0300

    MultipleVariables: green

--- Template/TemplateTest/VariablesTest.cs
+++ Template/TemplateTest/VariablesTest.cs
@@ -41,5 +41,35 @@
                 
             }
         }
+
+        [Fact]
+        public void MultipleVariables()
+        {
+            // arrange
+            var vars = new Variable[]
+                            {
+                                new Variable("firstVar", ArgumentType.Boolean),
+                                new Variable("secondVar", ArgumentType.DateTime),
+                                new Variable("thirdVar", ArgumentType.Double),
+                                new Variable("fourthVar", ArgumentType.Integer),
+                                new Variable("fifthVar", ArgumentType.Long), 
+                                new Variable("sixthVar", ArgumentType.String),
+                            };
+            var templateText = "text[%code;%]";
+
+            var language = new EchoLanguageStub();
+            
+            // act
+            using (var template = new Template(language, templateText, null, vars[0], vars[1], vars[2], vars[3], vars[4], vars[5]))
+            using (var output = new StringWriter())
+            {
+                template.Render(output);
+
+                // assert
+                var expected =
+                    "begin method(MyBoolean firstVar, MyDateTime secondVar, MyDouble thirdVar, MyInteger fourthVar, MyLong fifthVar, MyString sixthVar){ output(\"text\");code; } end";
+                Assert.Equal(expected, output.ToString());
+            }
+        }
     }
 }
